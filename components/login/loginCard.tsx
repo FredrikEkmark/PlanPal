4ccacecb11bfbe0184fbce3cb5dbf0de2ad84833
CrymoreX <112933611+CrymoreX@users.noreply.github.com>
2023-04-19 11:55:30 +0200

    jobbat vidare med login page

--- components/login/loginCard.tsx
+++ components/login/loginCard.tsx
@@ -1,5 +1,8 @@
 import { NextPage } from "next"
 import Image from "next/image"
+import Link from "next/link"
+
+import Button from "../basic/button"
 interface Props {}
 
 const LoginCard = ({}) => {
@@ -12,12 +15,46 @@ const LoginCard = ({}) => {
             alt={"#"}
             width={"200"}
             height={"85"}
-            className={""}
           ></Image>
         </div>
       </div>
       <div className="flex justify-center text-2xl font-semibold font mt-7 ">
         <h1>Planning made easy!</h1>
+      </div>
+
+      <div className="mt-10 ">
+        <input
+          className="box-border border-b-2 border-[none] border-solid;"
+          type="Email"
+          placeholder="Email"
+        />
+      </div>
+
+      <div>
+        <input
+          className="box-border border-b-2 border-[none] border-solid;"
+          type="Password"
+          placeholder="Password"
+        />
+      </div>
+
+      <Button
+        className=" mt-10 box-border flex flex-row justify-center items-center gap-2.5 absolute w-[90%] h-14 border p-2.5 rounded-[5px] border-solid border-black left-4 top-[440px];"
+        color={"green"}
+      >
+        Log in with email
+      </Button>
+
+      <div className="mt-40 ">
+        <p className="text-center ">
+          No account?
+          <Link
+            className=" box-border border-b-2 border-[none] border-solid;"
+            href={"/signup"}
+          >
+            Create one here!
+          </Link>
+        </p>
       </div>
     </>
   )
